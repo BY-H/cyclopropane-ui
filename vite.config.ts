4d0d5288f9297f99835600038022cf97cf3140b5
炳源 <bingyuango@gmail.com>
2024-12-21 19:23:48 +0800

    chore: 更改配置文件中对应base

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    base: './',
     server: {
         host: '0.0.0.0',
         port: 8080
